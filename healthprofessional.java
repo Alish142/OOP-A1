2a776a47aa3afebe4119f10765db7a698a8188f0
Alish142 <a.ranabhat.12@student.scu.edu.au>
2025-07-22 18:30:54 +1000

    Update healthprofessional.java

--- healthprofessional.java
+++ healthprofessional.java
@@ -1,13 +1,19 @@
-public class healthprofessional {
+public class Healthprofessional {
     protected int id ;
     protected String name ;
     protected String qualification;
 
 
-    public healthprofessional(){
+    public Healthprofessional(){
         this.id = 0;
         this.name = "Unknown";
         this.qualification = "Not specified";
+    }
+
+    public Healthprofessional(int id, String name, String qualification){
+        this.id = id;
+        this.name = name;
+        this.qualification = qualification;
     }
 
     
