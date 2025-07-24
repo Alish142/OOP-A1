6b5bcdf33319bfe2f63a4bd990af10d4c99edc74
Alish142 <a.ranabhat.12@student.scu.edu.au>
2025-07-24 19:13:39 +1000

    Update Physician.java

--- Physician.java
+++ Physician.java
@@ -1,3 +1,16 @@
-public class Physiotherapist {
+public class Physician extends HealthProfessional {
+     private String specialty;
+
+     public Physician(){
+        super();
+        this.specialty = "Not specified";
+     }
+
+     public Physician(int id, String name, String qualification, String specialty) {
+        super(id, name, qualification); 
+        this.specialty = specialty;
+    }
+
     
 }
+
