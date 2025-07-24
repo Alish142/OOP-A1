ee48b66cfcc004af7e59ff329ced4c05590761dc
Alish142 <a.ranabhat.12@student.scu.edu.au>
2025-07-24 20:10:50 +1000

    Update Appointment.java

--- Appointment.java
+++ Appointment.java
@@ -1,7 +1,16 @@
 public class Appointment {
-    private String pateintName;
+    private String patientName;
     private String mobile;
     private String timeSlot;
     private HealthProfessional doctor;
+
+    public Appointment(){
+        this.patientName = "Unknown";
+        this.mobile = "Unknown";
+        this.timeSlot = "Not set";
+        this.doctor = null;
+
+
+    }
     
 }
