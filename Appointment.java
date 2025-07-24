6019d8972cb51c724ce96e198263bdcdfe23f175
Alish142 <a.ranabhat.12@student.scu.edu.au>
2025-07-24 20:23:05 +1000

    Update Appointment.java

--- Appointment.java
+++ Appointment.java
@@ -1,24 +1,34 @@
 public class Appointment {
     private String patientName;
-    private String mobile;
-    private String timeSlot;
+    private String patientmobile;
+    private String preferredtimeSlot;
     private HealthProfessional doctor;
 
+    // Default constructer
     public Appointment(){
         this.patientName = "Unknown";
-        this.mobile = "Unknown";
-        this.timeSlot = "Not set";
+        this.patientmobile = "Unknown";
+        this.preferredtimeSlot = "Not set";
         this.doctor = null;
     }
 
+    //parameterized constructor 
+
     public Appointment(String patientName, String mobile, String timeSlot, HealthProfessional doctor){
         this.patientName = patientName;
-        this.mobile = mobile;
-        this.timeSlot = timeSlot;
+        this.patientmobile = mobile;
+        this.preferredtimeSlot = timeSlot;
         this.doctor = doctor;
     }
-
-
+    // print 
+    public void printAppointmentDetails(){
+        System.out.println("Patient Name:" + patientName);
+        System.out.println("Mobile: " + patientmobile);
+        System.out.println("Preferred Time Slot: " + preferredtimeSlot);
+        System.out.println("Doctor Details:");
+        if (doctor != null) {
+        doctor.printDetails();
+        } else {
+        System.out.println("No doctor assigned.");
+        }
     }
-    
-
