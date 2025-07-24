de805f3b0f1183c657841c37dcd331bbe59b3de9
Alish142 <a.ranabhat.12@student.scu.edu.au>
2025-07-24 19:16:23 +1000

    Update Physician.java

--- Physician.java
+++ Physician.java
@@ -11,6 +11,20 @@ public class Physician extends HealthProfessional {
         this.specialty = specialty;
     }
 
+    public String getSpecialty(){
+        return specialty;
+    }
+
+    public void setSepcialty(String specialty){
+        this.specialty = specialty;
+    }
+
+    @Override 
+    public void printDetails(){
+        System.out.println("Health Professional Type: Physician");
+        super.printDetails();  
+        System.out.println("Specialty: " + specialty);
+    }
+
     
 }
-
